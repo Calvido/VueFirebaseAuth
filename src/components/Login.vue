1e903ee87c5f020fc8576443f909ab75543a0dd1
Candido Roberto Florenca <candidoroberto@gmail.com>
2018-06-05 19:15:39 -0400

    Se cambia un poco el estilo de la tarjeta, quitando card-title y en su lugar se coloca un toolbar-title

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,48 +1,49 @@
 <template>
   <v-app>
-    <v-container>
-      <v-card >
-        <v-progress-linear :indeterminate="true" :hidden="!load"></v-progress-linear>
-        <v-card-title primary-title>
-          <div>
-            <h3 class="headline">Inicio de Sesión</h3>
-            <v-divider></v-divider>
-            <div>description</div>
-          </div>
-        </v-card-title>
-        <v-card-text>
-          <v-form v-model="valid" lazy-validation>
-            <v-text-field
-              v-model="email"
-              label="Correo Electronico"
-              id="email"
-              :rules="emailRules"
-              required
-              prepend-icon="email"
-            ></v-text-field>
-            <v-text-field
-              v-model="password"
-              label="Contraseña"
-              :append-icon="e1 ? 'visibility' : 'visibility_off'"
-              :append-icon-cb="() => (e1 = !e1)"
-              :type="!e1 ? 'password' : 'text'"
-              id="password"
-              :rules="passwordRules"
-              required
-              prepend-icon="vpn_key"
-            ></v-text-field>
-            <v-btn
-              :disabled="!valid"
-              color="info"
-              @click="login">Iniciar Sesión</v-btn>
-          </v-form>
-        </v-card-text>
-      </v-card>
+    <v-content>
+      <v-container>
+        <v-card>
+          <v-progress-linear :indeterminate="true" :hidden="!load"></v-progress-linear>
+          <v-toolbar>
+            <v-toolbar-title>Inicio de Sesión</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form v-model="valid" lazy-validation>
+              <v-text-field
+                v-model="email"
+                label="Correo Electronico"
+                id="email"
+                :rules="emailRules"
+                required
+                prepend-icon="email"
+              ></v-text-field>
+              <v-text-field
+                v-model="password"
+                label="Contraseña"
+                :append-icon="e1 ? 'visibility' : 'visibility_off'"
+                :append-icon-cb="() => (e1 = !e1)"
+                :type="!e1 ? 'password' : 'text'"
+                id="password"
+                :rules="passwordRules"
+                required
+                prepend-icon="vpn_key"
+              ></v-text-field>
+              <v-btn
+                :disabled="!valid"
+                color="info"
+                @click="login">Iniciar Sesión</v-btn>
+            </v-form>
+            <v-card-actions>
 
-      <pre style="padding-top:60px;">
-          {{ $data }}
-      </pre>
-    </v-container>
+            </v-card-actions>
+          </v-card-text>
+        </v-card>
+
+        <pre style="padding-top:60px;">
+            {{ $data }}
+        </pre>
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
