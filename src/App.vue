efbf76076a24a54ea7ddd3f5a0ca3ceee3c86e6e
Candido Roberto Florenca <candidoroberto@gmail.com>
2018-12-12 23:11:42 -0400

    Se actualiza el componente principal, se quitan las etiquetas innecesarias, y se comenta en el estilo el padding que no permitia centrar la pagina

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
-  <v-app>
-    <div id="app">
-      <img src="./assets/logo.png">
-      <router-view/>
-    </div>
-  </v-app>
+  <v-fade-transition appear>
+    <router-view/>
+  </v-fade-transition>
 </template>
 
 <script>
@@ -13,13 +10,13 @@
   };
 </script>
 
-<style>
+<style scoped>
   #app {
     /*font-family: "Avenir", Helvetica, Arial, sans-serif;*/
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    padding-top: 60px;
+    /*padding-top: 60px;*/
   }
 </style>
