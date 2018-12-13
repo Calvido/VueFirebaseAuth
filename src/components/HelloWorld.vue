85a752ff27b28a820be908592f8a05afd0d96f2a
Candido Roberto Florenca <candidoroberto@gmail.com>
2018-12-12 23:13:26 -0400

    Se agrega etiquetas necesarias para que el contenido se centre en la pantalla

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,57 @@
 <template>
   <v-app>
-    <div class="hello">
-      <h1>{{ msg }}</h1>
-      <h2>Essential Links</h2>
-      <ul>
-        <li>
-          <a href="https://vuejs.org" target="_blank">Core Docs</a>
-        </li>
-        <li>
-          <a href="https://forum.vuejs.org" target="_blank">Forum</a>
-        </li>
-        <li>
-          <a href="https://chat.vuejs.org" target="_blank">Community Chat</a>
-        </li>
-        <li>
-          <a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
-        </li>
-        <br>
-        <li>
-          <a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a>
-        </li>
-      </ul>
-      <h2>Ecosystem</h2>
-      <ul>
-        <li>
-          <a href="http://router.vuejs.org/" target="_blank">vue-router</a>
-        </li>
-        <li>
-          <a href="http://vuex.vuejs.org/" target="_blank">vuex</a>
-        </li>
-        <li>
-          <a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a>
-        </li>
-        <li>
-          <a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a>
-        </li>
-      </ul>
-    </div>
-    <br>
-    <v-container grid-list-xs>
-      <v-btn color="error" @click="logOut">Cerrar Sesión</v-btn>
-    </v-container>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <div>
+              <img src="../assets/logo.png">
+            </div>
+            <div class="hello">
+              <h1>{{ msg }}</h1>
+              <h2>Essential Links</h2>
+              <ul>
+                <li>
+                  <a href="https://vuejs.org" target="_blank">Core Docs</a>
+                </li>
+                <li>
+                  <a href="https://forum.vuejs.org" target="_blank">Forum</a>
+                </li>
+                <li>
+                  <a href="https://chat.vuejs.org" target="_blank">Community Chat</a>
+                </li>
+                <li>
+                  <a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
+                </li>
+                <br>
+                <li>
+                  <a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a>
+                </li>
+              </ul>
+              <h2>Ecosystem</h2>
+              <ul>
+                <li>
+                  <a href="http://router.vuejs.org/" target="_blank">vue-router</a>
+                </li>
+                <li>
+                  <a href="http://vuex.vuejs.org/" target="_blank">vuex</a>
+                </li>
+                <li>
+                  <a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a>
+                </li>
+                <li>
+                  <a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a>
+                </li>
+              </ul>
+            </div>
+            <br>
+            <v-container grid-list-xs>
+              <v-btn color="error" @click="logOut">Cerrar Sesión</v-btn>
+            </v-container>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
